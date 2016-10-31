a468f858e084697684fe8b6176497181a5ceacd3
pranayb001 <psborkar05@gmail.com>
2016-10-31 19:14:55 +0530

    Add files via upload

--- EM.m
+++ EM.m
@@ -3,79 +3,135 @@
 % EM, 3) K is the number of clusters 3) theta_old is cell having the thetas
 % of the previous steps 4) clst_rej are the clusters that were rejected in
 % the last step 5) clst_acc are the clusters that were accepted in
-% the last step 
-function [ theta , theta_old ] = EM( X, max_iter, K, theta_old, clst_rej, clst_acc )
-[N,~] = size(X);
+% the last step
+function [ theta_new ] = EM( X, max_iter, K,epsil, S, theta_old, clst_rej, clst_acc )
+[N,D] = size(X);
 mu = X( randsample(N,K),:);
 co_var_mat = cell(K);
 priors = ones(1,K)/K;
 q = repmat(ones(1,N)/K,K,1);
 maxIterCD = 500;
-[~,S]=size(theta_old);  %Calculate the number of previous iterations
-for i = 1:K
-    co_var_mat{i} = X.'*X;
+beta=1;
+if nargin == 8
+   [S,~,~]=size(theta_old);  %Calculate the number of previous iterations
 end
-P_h_given_x = zeros(K, N);
-N_ks = zeros(1,K);
-iter = 0;
-log_lik = [];
+co_var_mat_init = transpose(X)*X;
+for i = 1:K
+    co_var_mat{i} = co_var_mat_init;
+end
+iter = 1;
+% disp('All Variable initialized');
+% pause;
+log_lik = zeros(1,max_iter);
 while iter < max_iter
-   for i = 1:K
+   disp(iter);
+   %pause;
    %The E step
        %Calculate P(h|theta, x_j) for all j
-       for j = 1:N
-          P_h_given_x(i,j) = P_h_givn_x(j, X, co_var_mat{i}, priors(1,i), mu(i,:));
-       end
+       P_h_given_x = transpose(P_h_givn_x(X, K, priors, mu, co_var_mat));
        %implement stochastic co-ordinate descent using I_q_theta_thetaS and
        %KLDiv to get the 'q'(arbitrary probability) matrix
-       
-       iterCD = 0;
-       while (iterCD < maxIterCD) && sum(sum(abs(q-qTemp))) < epsilon
+   if nargin == 8
+      iterCD = 0;
+      qTemp = zeros(K,N);
+      disp(sum(sum(q)));
+      while (iterCD < maxIterCD)
           qTemp = q;
-          fun = @(x)beta*I_q_theta_thetaSNew( S, K, [q(:,1),q(:,2:N)], theta_old , X, clst_rej, clst_acc ) + alpha * KLDivNew(P_h_given_x,[q(:,1),q(:,2:N)]);
-          q(:,1) = fminsearch(fun,q(:,1));
+          r=q(:,1);
+          disp(beta);
+          fun = @(r)beta*I_q_theta_thetaSNew( S, K, [r,q(:,2:N)], theta_old , X, clst_rej, clst_acc ) + alpha * KLDivNew(P_h_given_x,[r,q(:,2:N)]);
+          r = fminunc(fun,r);
+          q(:,1)=r;
+
           for j = 2:N-1
-              fun = @(x)beta*I_q_theta_thetaSNew( S, K, [q(:,1:j-1),q(:,j),q(:,j+1:N)], theta_old , X, clst_rej, clst_acc ) + alpha * KLDivNew(P_h_given_x,[q(:,1:j-1),q(:,j),q(:,j+1:N)]);
-              q(:,j) = fminsearch(fun,q(:,j));
+             r = q(:,j);
+             fun = @(r)beta*I_q_theta_thetaSNew( S, K, [q(:,1:j-1),r,q(:,j+1:N)], theta_old , X, clst_rej, clst_acc ) + alpha * KLDivNew(P_h_given_x,[q(:,1:j-1),r,q(:,j+1:N)]);
+             r = fminunc(fun,r);
+             q(:,j)=r;
           end
-          fun = @(x)beta*I_q_theta_thetaSNew( S, K,  [q(:,1:N-1),q(:,N)], theta_old , X, clst_rej, clst_acc ) + alpha * KLDiv(P_h_given_x,[q(:,1:N-1),q(:,N)]);
-          q(:,N) = fminsearch(fun,q(:,N));
+          r = q(:,N);
+          fun = @(r)beta*I_q_theta_thetaSNew( S, K,  [q(:,1:N-1),r], theta_old , X, clst_rej, clst_acc ) + alpha * KLDiv(P_h_given_x,[q(:,1:N-1),r]);
+          r = fminunc(fun,r);
+          q(:,N)=r;
+          iterCD = iterCD + 1;
+          disp('loop');
+
       end
+      disp('after minimization');
+      disp(sum(sum(q)));
+      pause;
+   elseif nargin == 5
+       q = P_h_given_x;
+   else
+       disp('Wrong number of arguments in EM');
+   end
+   disp('E Step Done');
+   %pause;
    %the M Step
+   N_ks = zeros(1,K);
+   mu = zeros(K,D);
+   for i = 1:K
        % Update steps as a normal GMM
+       % Update the N_ks
+       N_ks(1,i) = sum(q(i,:));%kXN
        for j = 1:N
-          N_ks(1,i) = N_ks(1,i) + q(i,j);
           mu(i,:) = mu(i,:) + q(i,j)*X(j,:);
-          co_var_mat{i} = co_var_mat{i} + q(i,j)*(X(i,:) - mu(i,:))*(X(i,:) - mu(i,:)).';
-          delta = log_lik(1,-1) - log_lik(1,-2);
        end
        mu(i,:) = (1/N_ks(1,i))*mu(i,:);
+       co_var_mat{i} = zeros(D,D);
+       for j = 1:N
+          co_var_mat{i} = co_var_mat{i} + q(i,j)*(X(j,:) - mu(i,:)).'*(X(j,:) - mu(i,:));
+       end
        co_var_mat{i} = (1/N_ks(1,i))*co_var_mat{i};
        priors(1,i) = N_ks(1,i)/N;
    end
-   for i = 1:N
-       for j = 1:K
-           log_lik_iter = log_lik_iter + prior(1,j)*log(P_h_givn_x(j, X, co_var_mat{i}, priors(1,i), mu(i,:)));
-       end
-   end
+   disp('M Step Done');
+   %pause;
+   P_h_given_x = P_h_givn_x(X, K, priors, mu, co_var_mat);
+   log_lik_iter = sum(priors*transpose(P_h_given_x));
    % finding the log likelihood i.e $\sum_{i = 1}^{N}\sum_{j = 1}^{K}\pi_k*log(P(h,x|\theta)$
-   log_lik = [log_lik,log_lik_iter];
-   if iter > 2
-       if delta < epsilon
+   log_lik(1,iter) = log_lik_iter;
+   disp('Log Likelihood updated');
+   disp(log_lik(1,iter));
+   %pause;
+   if isnan(log_lik_iter)
+       disp(priors);
+       pause;
+       disp(P_h_given_x);
+       pause;
+   end
+   if iter > 3
+       delta = log_lik(1,iter) - log_lik(1,iter-2);
+       %disp(delta);
+       if delta < 0
+           disp('EM has converged');
+        %   pause;
            break;
        end
    end
+   iter = iter + 1;
 end
-theta = {K,3};
+%disp('here');
+%disp(log_lik);
+%disp('here');
+theta_new = {S,K,3};
 % returning thr parameters of the model in theta which is a cell of size
 % {K,3}
-for k = 1:K
-    theta{k,1} = co_var_mat{k}; %co variance matrix is stored here
-    theta{k,2} = mu(k,:); % the mean for a cluster
-    theta{k,3} = priors(1,k); % prior for a cluster
-end
-
 %append the now generated theta at the end of theta_old
-theta_old{S+1} = theta;
-
+if nargin == 8
+    for s = 1:S-1
+       theta_new{s} = theta_old{s};
+    end
+    for k = 1:K
+       theta_new{S,1,k} = co_var_mat{k}; %co variance matrix is stored here
+       theta_new{S,2,k} = mu(k,:); % the mean for a cluster
+       theta_new{S,3,k} = priors(1,k); % prior for a cluster
+    end
+else
+    for k = 1:K
+       theta_new{S,1,k} = co_var_mat{k}; %co variance matrix is stored here
+       theta_new{S,2,k} = mu(k,:); % the mean for a cluster
+       theta_new{S,3,k} = priors(1,k); % prior for a cluster
+    end
+end
 end
--- I_q_theta_thetaS.m
+++ I_q_theta_thetaS.m
@@ -5,17 +5,18 @@
 % clusters, data X, the indices of rejected and accepted clusters)
 function [ req_p ] = I_q_theta_thetaS( s, K, q,co_var_mat, prior, mu, X, clst_rej, clst_acc )
 req_p = 0;
+[N,~]=size(X);
+temp=size(clst_acc(s,:),1);
 for i = 1:K
     marg_prob_q = 0;
     for j = 1:N
-        marg_prob_q = marg_prob_q + q(1,j); 
-    end    
-    for j = 1:size(clst_acc(s,:),1)
+        marg_prob_q = marg_prob_q + q(1,j);
+    end
+    for j = 1:temp
         req_p = req_p - p_h_hs(q,co_var_mat(s,clust_acc(s,j)),prior(s,clust_acc(s,j)), X,mu(s,clust_acc(s,j)))*log(p_h_hs(q,co_var_mat(s,clust_acc(s,j)),prior(s,clust_acc(s,j)), X, mu(s,clust_acc(s,j)))/(marg_prob_h(X,co_var_mat(s,clst_acc(s,j)),prior(s,clst_acc(s,j)),mu(s,clst_acc(s,j)))*marg_prob_q));
     end
-    for j = 1:size(clst_rej(s,:),1)
+    for j = 1:temp
         req_p = req_p + p_h_hs(q,co_var_mat(s,clust_rej(s,j)),prior(s,clust_rej(s,j)), X,mu(s,clust_rej(s,j)))*log(p_h_hs(q,co_var_mat(s,clust_rej(s,j)),prior(s,clust_rej(s,j)), X, mu(s,clust_rej(s,j)))/(marg_prob_h(X,co_var_mat(s,clst_rej(s,j)),prior(s,clst_rej(s,j)),mu(s,clst_rej(s,j)))*marg_prob_q));
     end
-end    
 end
-
+end
